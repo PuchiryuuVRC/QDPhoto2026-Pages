b892fc42a1b30142d68e4a1b6f7aacffb6bf8b4d
PuchiryuuVRC <puchiryuuvrc@gmail.com>
2026-08-11 03:53:24 +0900

    Update site phase config (2026/08/11 03:53)

--- assets/site-phase-config.js
+++ assets/site-phase-config.js
@@ -1,1 +1,1 @@
-window.QD_PHOTO_PHASE_CONFIG = Object.freeze({"mode":"manual","manualPhase":2,"entryOpenAt":"2026-09-01T00:00:00+09:00","entryCloseAt":"2026-09-20T23:59:00+09:00","voteOpenAt":"2026-09-21T21:00:00+09:00","voteCloseAt":"2026-09-30T23:59:00+09:00","resultPublishAt":""});
+window.QD_PHOTO_PHASE_CONFIG = Object.freeze({"mode":"manual","manualPhase":1,"entryOpenAt":"2026-09-01T00:00:00+09:00","entryCloseAt":"2026-09-20T23:59:00+09:00","voteOpenAt":"2026-09-21T21:00:00+09:00","voteCloseAt":"2026-09-30T23:59:00+09:00","resultPublishAt":""});
